e20910ff1dd39b07eb5ad1bddb12c210daf9b074
osama-abdullah <osama2kabdullah@gmail.com>
2022-10-26 17:08:07 +0600

    generate token and if new user the user data save in databse

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,8 +10,46 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('', (req, res)=>{
-    res.send('travel guru running')
+const uri = `mongodb+srv://${process.env.TRAVEL_ADMIN_NAME}:${process.env.DATABASE_PASS_KEY}@cluster0.qf4bw47.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-app.listen(port, ()=>console.log('travel run on', port))
+async function run() {
+  try {
+    client.connect();
+    const places = client.db("travel-guru").collection("places");
+    const users = client.db("travel-guru").collection("users");
+
+    //get placces
+    app.get("/palces", async (req, res) => {
+      const result = await places.find().toArray();
+      res.send(result);
+    });
+
+    //post a user
+    app.put("/insertUser", async (req, res) => {
+      const query = { email: req.body.email };
+      const existUser = await users.findOne(query);
+      let result = {insert:false};
+      if (!existUser) {
+        const update = { $set: req.body };
+        const options = { upsert: true };
+        result = await users.updateOne(query, update, options);
+      }
+      const token = jwt.sign(query, process.env.JSON_TOKEN);
+      res.send({ token, result });
+    });
+  } finally {
+    //client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("", (req, res) => {
+  res.send("travel guru running");
+});
+
+app.listen(port, () => console.log("travel run on", port));
