adeda9b3402efc5a636a1a75b7ce87b5632fe602
osama-abdullah <osama2kabdullah@gmail.com>
2022-11-10 17:36:56 +0600

    re commit for find error

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; 
 
 app.use(cors());
 app.use(express.json());
